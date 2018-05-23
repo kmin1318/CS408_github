f55f227a8473f0c491a2a5903e1ce6f795652b75
kmin1318 <38938576+kmin1318@users.noreply.github.com>
2018-05-23 17:17:49 +0900

    Revert "0523_delete"
    This reverts commit 08911fd85b99e8b29e54ba3b04e704cca638fcc8.

--- 4 Emotion classifier/test5.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*
-
-import codecs
-with codecs.open("부산기장군수.txt", encoding='utf-8') as f:
-    data_test = [line.split('\t') for line in f.read().splitlines()]
-    
-print(len(data_test))
